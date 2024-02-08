9377b7359eeb84e4499aaac4544781f0428a9b91
firman-alam <aladiat046@gmail.com>
2024-02-08 20:59:35 +0700

    add redis module in server

--- server/src/redis.module.ts
+++ server/src/redis.module.ts
@@ -4,7 +4,9 @@ import {
   Module,
   ModuleMetadata,
 } from '@nestjs/common';
-import { Redis, RedisOptions } from 'ioredis';
+import IORedis, { Redis, RedisOptions } from 'ioredis';
+
+export const IORedisKey = 'IOREDIS';
 
 type RedisModuleOptions = {
   connectionOptions: RedisOptions;
@@ -20,12 +22,30 @@ type RedisAsyncModuleOptions = {
 
 @Module({})
 export class RedisModule {
-  static async registerAsync(): Promise<DynamicModule> {
+  static async registerAsync({
+    useFactory,
+    imports,
+    inject,
+  }: RedisAsyncModuleOptions): Promise<DynamicModule> {
+    const redisProvider = {
+      provide: IORedisKey,
+      useFactory: async (...args) => {
+        const { connectionOptions, onClientReady } = await useFactory(...args);
+
+        const client = await new IORedis(connectionOptions);
+
+        onClientReady(client);
+
+        return client;
+      },
+      inject,
+    };
+
     return {
       module: RedisModule,
-      imports: [],
-      providers: [],
-      exports: [],
+      imports,
+      providers: [redisProvider],
+      exports: [redisProvider],
     };
   }
 }
